94f275f0d769aa64bdf109ad9f8acf6d4822e44f
NkadimengKgothatso <2840850@Students.wits.ac.za>
2026-03-24 02:18:13 +0200

    finished the Tring mchine Simulator

---
+++ TuringMachineSimulator.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+using namespace std;
+
+
+
+string decoder(string machine){
+    string output;
+
+    for (int i = 0; i < machine.length(); i += 3){
+        string part = machine.substr(i, 3);
+
+        if (part == "000") output += "0";
+        else if (part == "001") output += "1";
+        else if (part == "010") output += "_";
+        else if (part == "100") output += "#";
+        else if (part == "101") output += ";";
+    }
+
+    return output;
+}
+
+
+struct Transition {
+    string nextState;
+    string writeSymbol;
+    string direction;
+};
+
+int main() {
+
+    string machine;
+    string input;
+
+    getline(cin, machine);
+    getline(cin, input);
+
+   
+    string decodedMachine = decoder(machine);
+
+   
+    vector<string> instru;
+    string part;
+    stringstream ss(decodedMachine);
+
+    while (getline(ss, part, ';')) {
+        if (part != "")
+            instru.push_back(part);
+    }
+
+    
+    map<pair<string, string>, Transition> delta;
+
+    for (string instr : instru) {
+        stringstream ss(instr);
+        vector<string> parts;
+        string p;
+
+        while (getline(ss, p, '#')) {
+            parts.push_back(p);
+        }
+
+        if (parts.size() < 5) continue;
+
+        string qi = parts[0];
+        string sn = parts[1];
+        string qj = parts[2];
+        string sm = parts[3];
+        string D  = parts[4];
+
+        delta[{qi, sn}] = {qj, sm, D};
+    }
+
+  
+    vector<string> tape;
+
+    if (input == "_") {
+        tape.push_back("_");
+    } else {
+        for (char c : input) {
+            tape.push_back(string(1, c));
+        }
+    }
+
+    string currentState = "0";
+    int head = 0;
+    int steps = 0;
+
+    
+    while (steps < 1000000) {
+
+        // ✅ SAFE tape expansion BEFORE access
+        if (head < 0) {
+            tape.insert(tape.begin(), "_");
+            head = 0;
+        }
+        if (head >= tape.size()) {
+            tape.push_back("_");
+        }
+
+        string currentSymbol = tape[head];
+
+        if (delta.find({currentState, currentSymbol}) == delta.end()) {
+            break;
+        }
+
+        Transition t = delta[{currentState, currentSymbol}];
+
+        // write
+        tape[head] = t.writeSymbol;
+
+        // move
+        if (t.direction == "00") head++;        // R
+        else if (t.direction == "10") head--;   // L
+        else if (t.direction == "0") {
+            // Stay
+        }
+
+        currentState = t.nextState;
+        steps++;
+
+        // halt if accept/reject
+        if (currentState == "1" || currentState == "10") break;
+    }
+
+    
+    int start = 0;
+    while (start < tape.size() && tape[start] == "_") start++;
+
+    int end = tape.size() - 1;
+    while (end >= 0 && tape[end] == "_") end--;
+
+    // if tape is all blanks
+    if (start > end) {
+        cout << "#" << currentState << "#_" << endl;
+        return 0;
+    }
+
+ 
+    string left = "";
+    string right = "";
+
+    for (int i = start; i <= end; i++) {
+        if (i < head) left += tape[i];
+        else right += tape[i];
+    }
+
+    cout << left << "#" << currentState << "#" << right << endl;
+
+    return 0;
+}
